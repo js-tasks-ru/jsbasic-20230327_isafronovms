a397bf25b599fa5fca687f2798ae7c38ac3c9b1b
Eugene Safronov <eugene.safronov@team.wrike.com>
2023-04-02 00:23:45 +0200

    Added 1-2 solution

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -2,9 +2,9 @@ function factorial(n) {
   // Basic check for an integer and a non-negative number
   if (n >= 0 && Number.isInteger(n)) {
     let factorial = 1; // The minimum value of the factorial
-    while (n > 1) factorial *= n--;
+    while (n > 1) {factorial *= n--;}
     return factorial;
   } else {
-    throw new Error('The factorial of a non-negative integer number')
+    throw new Error('The factorial of a non-negative integer number');
   }
 }
