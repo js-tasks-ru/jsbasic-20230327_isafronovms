8927f1a2429827527ea0f9b382e01d33002b39a2
Eugene Safronov <eugene.safronov@team.wrike.com>
2023-04-22 21:47:36 +0200

    Solved 3-3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
-  // ваш код...
+  let cssToJs = str.split('-');
+  for (let i = 1; i < cssToJs.length; i++) { 
+    cssToJs[i] = cssToJs[i].charAt(0).toUpperCase() + cssToJs[i].slice(1);
+  }
+  return cssToJs.join('');
 }
