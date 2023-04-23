b2c0da56ffa3ff65b5f9d0520b9c59de071519d6
Eugene Safronov <eugene.safronov@team.wrike.com>
2023-04-23 11:30:58 +0200

    Solved 4-3

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,6 +1,8 @@
 function makeDiagonalRed(table) {
   let cellHihlighted = 0;
   for (let i = 0; i < table.rows.length; i++) {
-    table.rows[i].cells[cellHihlighted++].style.backgroundColor = "red";
+    if (table.rows[i].cells[cellHihlighted]) {
+      table.rows[i].cells[cellHihlighted++].style.backgroundColor = "red";
+    } 
   }
 }
