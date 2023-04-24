ad9361badd4682e77f7bda37b9a23da01a95c7f5
Eugene Safronov <eugene.safronov@team.wrike.com>
2023-04-24 23:25:14 +0200

    Solved 5-3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector('.carousel__inner');
+  let slideWidth = document.querySelector('.carousel__slide').offsetWidth;
+  let slides = 4;
+  let activeSlide = 1;
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let transform = 0;
+
+  if (activeSlide == 1) {
+    arrowLeft.style.display = 'none';
+  } else if (activeSlide == slides) {
+    arrowRight.style.display = 'none';
+  }
+
+  arrowRight.addEventListener('click', (event) => {
+    transform -= slideWidth;
+    carousel.style.transform = `translateX(${transform}px)`;
+    activeSlide++; 
+    arrowLeft.style.display = 'flex';
+    if (activeSlide == slides) {
+      arrowRight.style.display = 'none';
+    } else {
+      arrowRight.style.display = 'flex';
+    }
+  });
+
+  arrowLeft.addEventListener('click', (event) => {
+    transform += slideWidth;
+    carousel.style.transform = `translateX(${transform}px)`;
+    activeSlide--; 
+    arrowRight.style.display = 'flex';
+    if (activeSlide == 1) {
+      arrowLeft.style.display = 'none';
+    } else {
+      arrowLeft.style.display = 'flex';
+    }
+  });
 }
